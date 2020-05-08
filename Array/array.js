fb96523c512befcc9a268bf7fdcc8917619fc9c3
CleuJunior <cleu.junior@gmail.com>
2020-05-08 01:14:07 -0300

    Arrays

---
+++ Array/array.js
@@ -0,0 +1,28 @@
+// Array de inteiros.
+
+let meusNumeros = [1, 3, 7];
+
+// Array de Strings.
+
+let minhaCores = ["Azul", "Preto", "Vermelho"];
+
+// Arrays de objestos.
+
+let pets = [{nome: "Peludo", especie: "cachorro"}, {nome: "Louro-José"}, {especie: "Papagaio"}];
+
+// Adicionando um valor na array.
+
+meusNumeros.push(8);
+
+// Imprimindo a array.
+
+console.log(meusNumeros);
+
+// Acessando uma array pelo index.
+
+console.log(minhaCores[2]);
+
+// Acessando uma array pelo index e pela chave.
+
+console.log(pets[0].especie);
+console.log(pets[1].nome);
