861c402d41c6e68ba1428a0e3585a33ab5eb5999
CleuJunior <cleu.junior@gmail.com>
2020-05-08 01:24:43 -0300

    Condicionais

---
+++ IF-ELSE/if_else.js
@@ -0,0 +1,23 @@
+let nota = 7.3;
+
+// Começo da Condicional.
+// Caso a condicional seja verdadeira tomará a primeira decisão
+// caso contrario tomará a segunda opção.
+
+if(nota > 6){
+    console.log("Boa nota.");
+} else{
+    console.log("Nota Vermelha!");
+}
+
+// Condicional while
+
+
+let contaDeDois = 2;
+
+// Contará de dois em dois até o número 10.
+
+while (contaDeDois <= 10) {
+    console.log(contaDeDois);
+    contaDeDois += 2;
+}
