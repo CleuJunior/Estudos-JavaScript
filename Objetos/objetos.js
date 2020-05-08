5b5d691fc285c7adb6fe21c479506dc5cc961567
CleuJunior <cleu.junior@gmail.com>
2020-05-08 00:47:54 -0300

    Criando um Objeto em JavaScript

---
+++ Objetos/objetos.js
@@ -0,0 +1,26 @@
+// Criando um objeto cachorro.
+
+let cachorro = {
+    nome: "Peludo",
+    idade: 5,
+    comida: {
+        preferida: "Ração",
+        menosGosta: "Cenoura"
+    },
+    cor: "Preto",
+    latir(){
+        return "AuAu";
+    }
+};
+
+// Chamando uma chave do objeto cachorro.
+
+console.log(cachorro.latir());
+console.log(cachorro.comida.preferida);
+console.log(cachorro.comida.menosGosta);
+
+document.addEventListener("click", funcaoClick);
+
+function funcaoClick() {
+    console.log("Fui Clickado");
+}
